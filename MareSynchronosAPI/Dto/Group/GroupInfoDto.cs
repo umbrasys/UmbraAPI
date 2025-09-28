3b175900c10a9a152a168b1bd6fee390aa58e8e0
Keda <sirconstance@pm.me>
2025-09-28 22:31:51 +0200

    Préparation API pour detection TypingState

---
+++ MareSynchronosAPI/Dto/Group/GroupInfoDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MareSynchronos.API.Data;
+using MareSynchronos.API.Data.Enum;
+using MessagePack;
+
+namespace MareSynchronos.API.Dto.Group;
+
+[MessagePackObject(keyAsPropertyName: true)]
+public record GroupInfoDto(GroupData Group, UserData Owner, GroupPermissions GroupPermissions) : GroupDto(Group)
+{
+    public GroupPermissions GroupPermissions { get; set; } = GroupPermissions;
+    public UserData Owner { get; set; } = Owner;
+
+    public string OwnerUID => Owner.UID;
+    public string? OwnerAlias => Owner.Alias;
+    public string OwnerAliasOrUID => Owner.AliasOrUID;
+
+    public bool IsTemporary { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+}
---
+++ MareSynchronosAPI/Dto/Group/GroupPasswordDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using MareSynchronos.API.Data;
+using MessagePack;
+
+namespace MareSynchronos.API.Dto.Group;
+
+[MessagePackObject(keyAsPropertyName: true)]
+public record GroupPasswordDto(GroupData Group, string Password) : GroupDto(Group)
+{
+    public bool IsTemporary { get; set; }
+    public DateTime? ExpiresAt { get; set; }
+}
