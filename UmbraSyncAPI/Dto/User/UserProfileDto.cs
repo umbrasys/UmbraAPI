a043bdafe08446cfaad76237a14ac57d78db69cc
Keda <sirconstance@pm.me>
2025-12-26 14:37:40 +0100

    Ajout du support des profils RP distincts avec gestion des images et descriptions séparées.

--- UmbraSyncAPI/Dto/User/UserProfileDto.cs
+++ UmbraSyncAPI/Dto/User/UserProfileDto.cs
@@ -4,4 +4,4 @@ using MessagePack;
 namespace UmbraSync.API.Dto.User;
 
 [MessagePackObject(keyAsPropertyName: true)]
-public record UserProfileDto(UserData User, bool Disabled, bool? IsNSFW, string? ProfilePictureBase64, string? Description) : UserDto(User);
+public record UserProfileDto(UserData User, bool Disabled, bool? IsNSFW, string? ProfilePictureBase64, string? Description, string? RpProfilePictureBase64 = null, string? RpDescription = null, bool? IsRpNSFW = null) : UserDto(User);
