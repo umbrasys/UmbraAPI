b8d443097ea8baeda8120362fb03c094c3c494ea
Keda <sirconstance@pm.me>
2025-12-27 11:26:20 +0100

    Ajout de la gestion des profils RP liés à des personnages avec les modèles, DTO et logique backend nécessaires.

--- UmbraSyncAPI/Dto/User/UserProfileDto.cs
+++ UmbraSyncAPI/Dto/User/UserProfileDto.cs
@@ -8,4 +8,5 @@ public record UserProfileDto(UserData User, bool Disabled, bool? IsNSFW, string?
     string? RpProfilePictureBase64 = null, string? RpDescription = null, bool? IsRpNSFW = null,
     string? RpFirstName = null, string? RpLastName = null, string? RpTitle = null, string? RpAge = null,
     string? RpHeight = null, string? RpBuild = null, string? RpOccupation = null, string? RpAffiliation = null,
-    string? RpAlignment = null, string? RpAdditionalInfo = null) : UserDto(User);
+    string? RpAlignment = null, string? RpAdditionalInfo = null,
+    string? CharacterName = null, uint? WorldId = null) : UserDto(User, CharacterName, WorldId);
